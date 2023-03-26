6c60a98eac7f845df757289eb836dd2342c7cda5
freeze0 <44439196+freeze0@users.noreply.github.com>
2023-03-26 15:51:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ n = int(n)
 k = 0
 max = 0
 d = dict()
+peresadki = dict()
+
 while True:
     line = f.readline()
     s = line.split()
@@ -12,18 +14,18 @@ while True:
     a = [i for i in range(int(s[2]), int(s[3]) + 1)]
     d[s[0] + s[1]] = a
 
+for i in range(1, n):
+    pass_count = 0
+    for value in d.values():
+        if i in value and i != value[len(value) - 1]:
+            pass_count += 1
+    if max < pass_count:
+        max = pass_count
+    peresadki[str(i) + '-' + str(i+1)] = pass_count
 
-#переделать
-for i in range(1, n + 1):
-    for key in d.keys():
-        if i in d.get(key):
-            k+=1
-        else:
-            k-=1
-        if k > max:
-            max = k
-
-
-print(max)
 print(d)
+print(peresadki)
+for key, value in peresadki.items():
+    if value == max:
+        print(key)
 f.close()
